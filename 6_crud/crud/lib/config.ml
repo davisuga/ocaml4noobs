e28377e15c6eed80b310b6c07c30ba0bd1b62e00
Davi William Moraes Suga <daviciencia1@gmail.com>
2022-09-17 13:58:34 -0300

    feat: better env vars

--- 6_crud/crud/lib/config.ml
+++ 6_crud/crud/lib/config.ml
@@ -1,2 +1,7 @@
+(* Lê as variáveis do .env *)
 let _ = Dotenv.export ()
-let db_url = Sys.getenv_opt "DB_URL" |> Option.value ~default:"pg"
+
+(* Procura a variável DB_URL no ambiente, se não tiver retorna uma url local *)
+let db_url =
+  Sys.getenv_opt "DB_URL"
+  |> Option.value ~default:"postgresql://postgres:password@localhost:5432"
