cdbd5b8ede37ff0709f7b7c64af14e71829e8e5f
Davi William Moraes Suga <daviciencia1@gmail.com>
2022-09-17 13:57:49 -0300

    feat: basic router

--- 6_crud/crud/lib/server.ml
+++ 6_crud/crud/lib/server.ml
@@ -1,8 +1,6 @@
-
-let () =
+let start () =
   Dream.run
+  (* Adiciona o middleware de logs *)
   @@ Dream.logger
-  @@ Dream.router [
-    Dream.get "/users" (fun _ ->
-      Dream.html ("world"));
-  ]
+  (* ...e o de rotas *)
+  @@ Dream.router [ Dream.get "/" (fun _ -> Dream.html "Hello, world!") ]
